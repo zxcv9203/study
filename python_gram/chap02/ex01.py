113bbd90aa6bd01474d3f59efcab3589c10db138
yongckim <zxcv9203@naver.com>
2021-05-16 08:45:29 +0900

    print

---
+++ python_gram/chap02/ex01.py
@@ -0,0 +1,18 @@
+# Print 사용법
+
+# 기본출력
+print('aaa')
+print("aaa")
+print('''aaa''')
+print("""aaa""")
+
+# print만 입력시 개행문자 출력
+print()
+
+#separator 옵션
+#분리영역을 원하는 문자로 출력할 수 있음 (기본 값은 space)
+print('p', 'y', 't', 'h', 'o', 'n', sep='|')
+print('010', '1234', '5678', sep='-')
+print('python', 'gmail.com', sep='@')
+
+print()
